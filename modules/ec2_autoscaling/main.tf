da0005921defa37e3e82662ca27b4748f42dbb04
Aditya Anand <anandaditya701@gmail.com>
2024-12-13 13:20:57 +0530

    Update main.tf

--- modules/ec2_autoscaling/main.tf
+++ modules/ec2_autoscaling/main.tf
@@ -1,16 +1,9 @@
-resource "aws_launch_configuration" "main" {
-  name          = "${var.name}-launch-configuration"
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  user_data     = var.user_data
-  security_groups = var.security_group_ids
-}
-
-resource "aws_autoscaling_group" "main" {
-  name                = "${var.name}-autoscaling-group"
-  max_size            = var.max_size
-  min_size            = var.min_size
-  desired_capacity    = var.desired_capacity
-  vpc_zone_identifier = var.subnet_ids
-  launch_configuration = aws_launch_configuration.main.id
+module "autoscaling" {
+  source                = "./modules/ec2_autoscaling"
+  name                  = var.project_name
+  ami_id                = var.ami_id
+  instance_type         = var.instance_type
+  subnet_ids            = module.vpc.public_subnet_ids
+  security_group_ids    = [aws_security_group.main.id]
+  user_data             = file("${path.module}/startup.sh") # Correctly reference the startup script
 }
