3615ba81de2271b8ce1c416746a7e440b10679da
Aditya Anand <anandaditya701@gmail.com>
2024-12-13 13:13:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,4 @@
-variable "region" {
-  default = "us-east-1"
+variable "ami_id" {
+  description = "ami-005fc0f236362e99f"
+  type        = string
 }
-variable "vpc_cidr_block" {}
-variable "public_subnets_cidr_blocks" {
-  type = list(string)
-}
-variable "private_subnets_cidr_blocks" {
-  type = list(string)
-}
-variable "availability_zones" {
-  type = list(string)
-}
-variable "project_name" {}
-variable "ami_id" {}
-variable "instance_type" {}
